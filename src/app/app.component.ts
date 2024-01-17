f5b0a164d849f53cd527f03659142c08642f76fb
Florian <florian.drevet@epita.fr>
2024-01-17 15:07:40 +0100

    feat(simple_request): add fetch to endpoint hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import {HttpClientModule} from "@angular/common/http";
 import {ItemListComponent} from "./item-list/item-list.component";
 import {AppModule} from "./app.module";
+import {DataService} from "./data.service";
 
 @Component({
   selector: 'app-root',
@@ -14,4 +15,17 @@ import {AppModule} from "./app.module";
 })
 export class AppComponent {
   title = 'archi-frontend';
+  data: any;
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getData().subscribe(
+      (response) => {
+        this.data = response;
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des données', error);
+      }
+    );
+  }
 }
