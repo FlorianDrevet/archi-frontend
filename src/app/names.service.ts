70d4018d970769864e984ffb8345bf6a1cdd07db
Florian <florian.drevet@epita.fr>
2024-01-18 01:30:05 +0100

    feat(names): add list names

--- src/app/names.service.ts
+++ src/app/names.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {NamesInterface} from "./shared/interfaces/names.interface";
+import {catchError, tap, throwError} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,26 @@ export class NamesService {
   constructor(private http: HttpClient) { }
 
   getNames() {
-    return this.http.get<NamesInterface>(`${this.apiUrl}/names`);
+    return this.http.get<NamesInterface>(`${this.apiUrl}/names`).pipe(
+      tap(response => {
+        console.log('Response:', response);
+      }),
+      catchError(error => {
+        console.error('Error:', error);
+        return throwError(error);
+      })
+    );
   }
 
   postName(name: string) {
-    return this.http.post(`${this.apiUrl}/name`, { name });
+    return this.http.post(`${this.apiUrl}/name`, { name }).pipe(
+      tap(response => {
+        console.log('Response:', response);
+      }),
+      catchError(error => {
+        console.error('Error:', error);
+        return throwError(error);
+      })
+    );
   }
 }
